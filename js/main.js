f0f6e7ae9d87be7fbdfc28dfdd35272078645117
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-10-15 03:40:26 -0400

    created anchor links and dealt with fixed navbar offset

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(window).scroll(function() {     
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+        $("#nav").addClass("shadow");
+    }
+    else {
+        $("#nav").removeClass("shadow");
+    }
+});
+
+// $(document).ready(function(){
+//     // Add smooth scrolling to all links
+//     $("a").on('click', function(event) {
+  
+//       // Make sure this.hash has a value before overriding default behavior
+//       if (this.hash !== "") {
+//         // Prevent default anchor click behavior
+//         event.preventDefault();
+  
+//         // Store hash
+//         var hash = this.hash;
+  
+//         // Using jQuery's animate() method to add smooth page scroll
+//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         }, 800, function(){
+  
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         });
+//       } // End if
+//     });
+//   });
